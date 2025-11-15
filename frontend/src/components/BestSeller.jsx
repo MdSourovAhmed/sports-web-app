9498a4b154ca68af274483c10d5a44c1c92f6648
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-11-15 23:59:52 +0600

    fixing the bestsell page

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import sportsData from "../assets/sportsData.js";
-// import Title from './Title.jsx'
-// import ProductItem from "./ProductItem.jsx";
-
-// const BestSeller = () => {
-//   const Products = sportsData.filter((it) => it.sport == "badminton");
-//   return (
-//     <div className="my-10">
-//       <div className="text-center text-3xl py-8 ">
-//         <Title t1={'BEST'} t2={'SELLER'} />
-//         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">
-//           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
-//           omnis, sint fugit facilis rem quia.
-//         </p>
-//       </div>
-//       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-//         {Products.map((item, index) => (
-//           <ProductItem
-//             key={index}
-//             id={index}
-//             image={item.image}
-//             name={item.name}
-//             price={item.price}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BestSeller;
-
-// BestSeller.jsx (Updated)
 import React, { useState, useEffect } from "react";
 import Title from "./Title.jsx";
 import ProductItem from "./ProductItem.jsx";
