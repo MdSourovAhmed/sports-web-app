a02cbb48b3bf03faec232df1af14a7de1b71501c
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-12-07 23:57:36 +0600

    Some minor changes

--- frontend/src/contexts/ShopContext.jsx
+++ frontend/src/contexts/ShopContext.jsx
@@ -1,21 +1,3 @@
-// // import { createContext } from "react";
-// // // import products from '../assets/products'
-
-// // export const ShopContext=createContext();
-
-// // const ShopContextProvider=(props)=>{
-// //     const values={
-
-// //     };
-// //     return (
-// //         <ShopContext.Provider value={values}>
-// //             {props.children}
-// //         </ShopContext.Provider>
-// //     )
-// // }
-
-// // export default ShopContextProvider;
-
 // import React, { createContext, useState, useMemo } from "react";
 // // import products from '../assets/products'
 
