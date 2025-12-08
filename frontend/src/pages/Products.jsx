62b77422181c439739e035193672ae54dbd10559
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-12-08 23:58:51 +0600

    Some minor changes

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -821,13 +821,6 @@
 
 
 
-
-
-
-
-
-
-
 import React, { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { ShopContext } from "../contexts/ShopContext";
