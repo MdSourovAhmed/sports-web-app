63d4f409ca74c13c80e95901fd687b93be4628fc
MdSourovAhmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-12-20 22:30:14 +0600

    some changes

--- frontend/src/components/LatestCollections.jsx
+++ frontend/src/components/LatestCollections.jsx
@@ -34,36 +34,6 @@
 
 // export default LatestCollections;
 
-// const LatestCollections = () => {
-//   const Products = sportsData.slice(0, 10);
-
-//   return (
-//     <div className="my-10">
-//       <div className="text-center py-8 text-3xl">
-//         <Title t1={"LATEST"} t2={"COLLECTIONS"} />
-//         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa aut
-//           vitae suscipit recusandae omnis, minima quisquam esse cum tenetur
-//           commodi.
-//         </p>
-//       </div>
-
-//       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-//         {Products.map((item, index) => (
-//           <ProductItem
-//             key={index}
-//             id={index}
-//             image={item.image}
-//             name={item.name}
-//             price={item.price}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LatestCollections;
 
 
 import React, { useState, useEffect } from "react";
